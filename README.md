06f52bf3c22ef5ef6ba7db16c52ca1a6eea33bf5
Matheus <msousa@sjtu.edu.cn>
2024-12-21 18:17:08 +0800

    Improve CLI

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 Given a list of eigenvalues, computes the density of states.
 
+```bash
+$ ./dos --help
+Usage: ./dos [OPTION]... FILE
+Calculate density of states from eigenvalues in FILE.
+
+Options:
+  -s, --sigma=VALUE      set the broadening (default: 0.05)
+  -p, --padding=VALUE    set the padding (default: 0.10)
+  -n, --npoints=NUM      set number of points (default: 1000)
+  -h, --help             display this help and exit
+
+Report bugs to: <https://github.com/keyehzy/dos.c/issues>
+```
+
 ## Example
 
 Example using the eigenvalues of a [simple 1d system](test.c):
@@ -11,10 +25,6 @@ make
 ./test > test.dat
 ./dos test.dat > output
 ```
-
-## Options
-
-Options can be changed using preprocessor directives in the [source code](dos.c) itself.
 
 # License
 
--- dos.c
+++ dos.c
@@ -4,40 +4,11 @@
 #include <stdint.h>
 #include <string.h>
 #include <math.h>
-
+#include <getopt.h>
 
 #define sizeof(x)    (ptrdiff_t) sizeof(x)
 #define countof(a)   (sizeof(a) / sizeof(*(a)))
 #define lengthof(s)  (countof(s) - 1)
-
-
-typedef struct {
-  char *beg;
-  char *end;
-} arena;
-
-arena newarena(ptrdiff_t cap) {
-  arena a = {0};
-  a.beg = malloc(cap);
-  a.end = a.beg ? a.beg+cap : 0;
-  return a;
-}
-
-__attribute((malloc, alloc_size(2, 4), alloc_align(3)))
-void *alloc(arena *a, ptrdiff_t size, ptrdiff_t align, ptrdiff_t count)
-{
-    ptrdiff_t padding = -(uintptr_t)a->beg & (align - 1);
-    ptrdiff_t available = a->end - a->beg - padding;
-    if (available < 0 || count > available / size) {
-        abort();
-    }
-    void *p = a->beg + padding;
-    a->beg += padding + count*size;
-    return memset(p, 0, count*size);
-}
-
-#define NEW(a, t, n)  (t *) alloc(a, sizeof(t), _Alignof(t), n)
-
 
 typedef struct {
   char *data;
@@ -56,26 +27,10 @@ typedef struct {
 typedef struct {
   double *data;
   ptrdiff_t len;
-  ptrdiff_t cap;
 } fltlist;
 
 
-#define MAX(x, y) (((x) > (y)) ? (x) : (y))
-
-void list_push(fltlist *lst, double item, arena *a) {
-  if (lst->len + 1 >= lst->cap) {
-    ptrdiff_t newcap = MAX(1, 2 * lst->cap);
-    double   *oldptr = lst->data;
-    double   *newptr = NEW(a, double, newcap);
-    memcpy(newptr, oldptr, sizeof(double) * lst->len);
-    lst->data = newptr;
-    lst->cap  = newcap;
-  }
-  lst->data[lst->len++] = item;
-}
-
-
-static str readcontent(str path, arena *a) {
+static str readcontent(str path) {
   char *fcontent = NULL;
   int fsize = 0;
   FILE *fp;
@@ -91,7 +46,7 @@ static str readcontent(str path, arena *a) {
     fsize = ftell(fp);
     rewind(fp);
 
-    fcontent = NEW(a, char, fsize);
+    fcontent = malloc(sizeof(char) * fsize);
     fread(fcontent, 1, fsize, fp);
 
     fclose(fp);
@@ -125,15 +80,32 @@ static double to_double(str s) {
     return strtod(buf, NULL);
 }
 
-fltlist getitems(str path, arena *perm) {
-  strpair pair = {0};
-  pair.tail = readcontent(path, perm);
+static ptrdiff_t count_lines(str content) {
+  ptrdiff_t count = 0;
+  str remaining = content;
+
+  while (remaining.len) {
+    remaining = chop(remaining, '\n').tail;
+    count++;
+  }
+
+  return count;
+}
+
+static fltlist getitems(str path) {
+  str content = readcontent(path);
+  ptrdiff_t num_lines = count_lines(content);
 
   fltlist lst = {0};
+  lst.data = malloc(sizeof(double) * num_lines);
+  lst.len = 0;
+
+  strpair pair = {0};
+  pair.tail = content;
 
   while (pair.tail.len) {
     pair = chop(pair.tail, '\n');
-    list_push(&lst, to_double(pair.head), perm);
+    lst.data[lst.len++] = to_double(pair.head);
   }
 
   return lst;
@@ -155,66 +127,132 @@ static void find_min_max(fltlist lst, double *min, double *max) {
   }
 }
 
-#define NUM_POINTS 1000
-#define SIGMA 0.05
-#define PADDING_FACTOR 0.1
 #define PI 3.14159265358979323844
 
-void print_dos(fltlist lst, arena *a) {
+void print_dos(fltlist lst, double sigma, double padding_factor, ptrdiff_t num_points) {
   double min_val;
   double max_val;
   find_min_max(lst, &min_val, &max_val);
 
-  double padding = (max_val - min_val) * PADDING_FACTOR;
+  double padding = (max_val - min_val) * padding_factor;
   min_val -= padding;
   max_val += padding;
 
-  double dE       = (max_val - min_val) / (double)(NUM_POINTS - 1);
+  double dE = (max_val - min_val) / (double)(num_points - 1);
 #if defined(LORENTZ_KERNEL)
-  double norm     = 1.0 / PI / SIGMA;
+  double norm = 1.0 / PI / sigma;
 #else
-  double norm     = 1.0 / sqrt(2.0 * PI) / SIGMA;
+  double norm = 1.0 / sqrt(2.0 * PI) / sigma;
 #endif
-  double integral = 0.0;
 
-  double *E_values   = NEW(a, double, NUM_POINTS);
-  double *DOS_values = NEW(a, double, NUM_POINTS);
+  double integral    = 0;
+  double* DOS_values = malloc(sizeof(double) * num_points);
 
-  for (ptrdiff_t i = 0; i < NUM_POINTS; i++) {
+#pragma omp parallel for
+  for (ptrdiff_t i = 0; i < num_points; i++) {
     double E = min_val + (double)i * dE;
     double rho = 0.0;
 
     for (ptrdiff_t j = 0; j < lst.len; j++) {
-      double delta_E = (E - lst.data[j]) / SIGMA;
+      double delta_E = (E - lst.data[j]) / sigma;
 #if defined(LORENTZ_KERNEL)
       rho += 1.0 / (1.0 + delta_E * delta_E);
 #else
       rho += exp(-0.5 * delta_E * delta_E);
 #endif
     }
-
-    E_values[i] = E;
     DOS_values[i] = rho * norm;
-    integral += rho * norm * dE;
+    integral     += rho * norm * dE;
   }
 
-  for (ptrdiff_t i = 0; i < NUM_POINTS; ++i) {
-    printf("%lf %lf\n", E_values[i], DOS_values[i] / integral);
+   for (ptrdiff_t i = 0; i < num_points; ++i) {
+    double E = min_val + (double)i * dE;
+    printf("%lf %lf\n", E, DOS_values[i] / integral);
   }
 }
 
+#define SIGMA 0.05
+#define PADDING_FACTOR 0.1
+#define NUM_POINTS 1000
+
+static void usage(const char *progname, int status) {
+  fprintf(status ? stderr : stdout,
+          "Usage: %s [OPTION]... FILE\n"
+          "Calculate density of states from eigenvalues in FILE.\n"
+          "\n"
+          "Options:\n"
+          "  -s, --sigma=VALUE      set the broadening (default: %.2f)\n"
+          "  -p, --padding=VALUE    set the padding (default: %.2f)\n"
+          "  -n, --npoints=NUM      set number of points (default: %d)\n"
+          "  -h, --help             display this help and exit\n"
+          "\n"
+          "Report bugs to: <https://github.com/keyehzy/dos.c/issues>\n",
+          progname, SIGMA, PADDING_FACTOR, NUM_POINTS);
+  exit(status);
+}
+
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr,
-            "Usage: %s [OPTION]... FILE\n"
-            "Calculate density of states from eigenvalues in FILE.\n",
-            argv[0]);
+  static struct option long_options[] = {
+    {"sigma",   required_argument, 0, 's'},
+    {"padding", required_argument, 0, 'p'},
+    {"npoints", required_argument, 0, 'n'},
+    {"help",    no_argument,       0, 'h'},
+    {0, 0, 0, 0}
+  };
+
+  double sigma   = SIGMA;
+  double padding = PADDING_FACTOR;
+  int npoints    = NUM_POINTS;
+
+  while (1) {
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "s:n:h", long_options, &option_index);
+
+    if (c == -1) break;
+
+    switch (c) {
+    case 's':
+      sigma = strtod(optarg, NULL);
+      if (sigma <= 0) {
+        fprintf(stderr, "%s: sigma must be positive\n", argv[0]);
+        return 1;
+      }
+      break;
+
+    case 'p':
+      padding = strtod(optarg, NULL);
+      if (padding <= 0) {
+        fprintf(stderr, "%s: padding must be positive\n", argv[0]);
+        return 1;
+      }
+      break;
+
+    case 'n':
+      npoints = strtol(optarg, NULL, 10);
+      if (npoints <= 0) {
+        fprintf(stderr, "%s: number of points must be positive\n", argv[0]);
+        return 1;
+      }
+      break;
+
+    case 'h':
+      usage(argv[0], 0);
+      break;
+
+    case '?':
+      usage(argv[0], 1);
+      break;
+    }
+  }
+
+  if (optind >= argc) {
+    fprintf(stderr, "%s: missing input file\n", argv[0]);
+    fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
     return 1;
   }
 
-  arena perm = newarena(1<<15);
-  str path = STR(argv[1], strlen(argv[1]));
-  fltlist items = getitems(path, &perm);
-  print_dos(items, &perm);
+  str path = STR(argv[optind], strlen(argv[optind]));
+  fltlist items = getitems(path);
+  print_dos(items, sigma, padding, npoints);
   return 0;
 }
